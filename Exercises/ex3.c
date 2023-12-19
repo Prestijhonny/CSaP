d780a2c5fb8f5c8d971789d87582e353c122ba54
Prestijhonny <wprestigiovanni@yahoo.it>
2023-12-19 15:12:16 +0100

    New ex3

---
+++ Exercises/ex3.c
@@ -0,0 +1,23 @@
+//Create a signal
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h> 
+#include <unistd.h>
+
+void handler(int value){
+    printf("Hi i'm a signal!\n");
+    printf("Caught signal %d\n", value);
+}
+
+int main()
+{
+    if (signal(SIGUSR1, handler) == SIG_ERR){
+        printf("Error to create a signal\n");
+    }
+    // kill(getpid(),SIGUSR1);
+    
+    exit(0);
+}
+
+// gcc ex3.c -o ex3;./ex3
